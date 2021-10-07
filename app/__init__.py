ef28e64edeb56af3709cb2bedf892629e02c49a5
Chee Wai <cheewai@movingmouse.com>
2021-10-07 16:23:59 +0800

    added protected routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,22 @@ from .main.controller.auth_controller import api as auth_ns
 
 
 blueprint = Blueprint('api', __name__)
+authorizations = {
+    'auth': {
+        'type': 'apiKey',
+        'in': 'header', 
+        'name': 'Authorization'
+    }
+}
+
 api = Api(blueprint,
         title='flask boilerplate',
         version='1.0',
-        description='flask boilerplate only'
+        description='flask boilerplate only',
+        authorizations=authorizations
     )
 
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns, path='/auth')
+
+
---
+++ app/main/util/decorator.py
@@ -0,0 +1,32 @@
+from functools import wraps
+from flask import request
+
+from app.main.service.auth_helper import Auth
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        data, status = Auth.get_logged_in_user(request)
+        token = data.get('data')
+        if not token:
+            return data, status
+        return f(*args, **kwargs, payload=token)
+    return decorated
+
+
+def admin_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        data, status = Auth.get_logged_in_user(request)
+        token = data.get('data')
+        if not token:
+            return data, status
+        admin = token.get('admin')
+        if not admin:
+            return {
+                'status': 'fail',
+                'message': 'admin required'
+            }, 401
+        return f(*args, **kwargs)
+    return decorated
